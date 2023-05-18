35a3e700cfafd7be8cbdbffbae1c2acea9a6d6e5
MiMi <janicehsu890228@gmail.com>
2023-05-18 20:47:38 +0800

    首頁

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <button @click="$router.push({ name: 'learning-corner' })">
-      前往學習區
-    </button>
+    <div class="hero-container">
+      <img class="decoration-1" src="../assets/decorations/hero-1.svg" alt="">
+      <img class="decoration-2" src="../assets/decorations/hero-2.svg" alt="">
+      <img class="decoration-3" src="../assets/decorations/hero-3.svg" alt="">
+      <img class="hero-img" src="../assets/images/home/hero.jpg" alt="">
+      <a class="go-to-introduction" href="./learning-corner">
+        <div>
+          開始探索
+        </div>
+      </a>
+    </div>
+
   </div>
 </template>
 
@@ -22,12 +31,48 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/scss/all.scss";
 
-div {
-  button {
-    position: absolute;
-    transform: translate(-50%, -50%);
-    top: 20%;
-    right: 10%;
+div.hero-container {
+
+  a.go-to-introduction {
+    display: inline-block;
+    font-size: $sub-info;
+    color: $primary-black;
+
+
+
+    div {
+      width: 100px;
+      height: 50px;
+      background-color: $primary-white;
+      border: 5px solid $primary-blue;
+      border-radius: 30px;
+      position: absolute;
+      transform: translate(-50%, -50%);
+      top: 20%;
+      right: 10%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &:link {
+        color: $primary-black;
+      }
+
+      &:visited {
+        color: $primary-black;
+      }
+
+      &:hover {
+        color: $primary-white;
+        background-color: $primary-blue;
+      }
+
+      &:active {
+        color: $primary-white;
+        background-color: $primary-blue;
+        opacity: 0.5;
+      }
+    }
   }
 }
 </style>
