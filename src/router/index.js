c81b2fb21bfb6600321c30525f66c161e1c4e0b5
Incheat <incheatwork@gmail.com>
2023-05-18 17:08:11 +0900

    try to fix router error

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { publicPath } from '../vue.config'
 import MainView from "@/views/MainView.vue";
 import TestView from "@/views/TestView.vue";
 
@@ -36,7 +37,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  base: process.env.NODE_ENV === "production" ? "/yola" : "",
+  base: publicPath === "/yola/" ? "/yola" : "",
   routes,
 });
 
