42ac29ebecb094abed32815d3229f8205a85da85
Incheat <incheatwork@gmail.com>
2023-05-18 17:02:36 +0900

    update router base to fix deploy error

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  base: publicPath,
+  base: process.env.NODE_ENV === "production" ? "/yola" : "",
   routes,
 });
 
