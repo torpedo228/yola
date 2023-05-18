6abe0e992a186d6c195a2ee94a0e60647820ca2f
Incheat <incheatwork@gmail.com>
2023-05-18 17:17:31 +0900

    try to fix path error

--- src/router/index.js
+++ src/router/index.js
@@ -41,7 +41,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   // base: publicPath === "/yola/" ? "/yola" : "",
   routes,
 });
