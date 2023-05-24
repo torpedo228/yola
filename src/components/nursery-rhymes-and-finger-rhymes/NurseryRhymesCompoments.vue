e6ee3a8e631c89fad166c881627ab9679fdcf951
MiMi <janicehsu890228@gmail.com>
2023-05-25 00:02:30 +0800

    上傳

---
+++ src/components/nursery-rhymes-and-finger-rhymes/NurseryRhymesCompoments.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nursey-rythmes-compoments-container" id="nursey-rythmes">
+
+
+    <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 547.74 547.74" xml:space="preserve" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M273.87,345.132c39.293,0,71.262-31.969,71.262-71.262c0-39.293-31.969-71.261-71.262-71.261 c-39.293,0-71.261,31.968-71.261,71.261C202.609,313.163,234.577,345.132,273.87,345.132z M273.87,228.735 c24.927,0,45.135,20.208,45.135,45.135s-20.208,45.135-45.135,45.135s-45.135-20.208-45.135-45.135 S248.943,228.735,273.87,228.735z"></path> <path d="M273.87,547.74c151.256,0,273.87-122.617,273.87-273.87S425.126,0,273.87,0S0,122.617,0,273.87 S122.614,547.74,273.87,547.74z M273.87,47.43c124.86,0,226.44,101.58,226.44,226.44H485.01 c0-116.424-94.716-211.14-211.14-211.14V47.43z M273.87,116.051c87.023,0,157.822,70.799,157.822,157.819h-15.3 c0-78.587-63.936-142.52-142.522-142.52V116.051z M273.87,187.309c47.729,0,86.562,38.832,86.562,86.562 c0,47.729-38.832,86.562-86.562,86.562c-47.73,0-86.562-38.832-86.562-86.562C187.309,226.14,226.14,187.309,273.87,187.309z"></path> </g> </g> </g></svg>
+
+  </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() { },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/scss/all.scss";
+
+div.hot-rythmes-container {
+
+  .color
+}
+</>
--- src/views/sections/NurseryRhymesAndFingerRhymes.vue
+++ src/views/sections/NurseryRhymesAndFingerRhymes.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="container">
     <BreadCrumb />
-      <SectionTitle
-        title="兒歌 Nursery Rhymes"
-        :imgSrc="require('@/assets/icons/logo/nursery-rhymes-logo.svg')"
-        alt="兒歌logo"
-      />
-      <SectionTitle
-        title="手指謠 Finger Rhymes"
-        :imgSrc="require('@/assets/icons/logo/finger-rhymes-logo.svg')"
-        alt="手指謠logo"
-      />
-      <router-view />
-    </div>
+    <SectionTitle title="兒歌 Nursery Rhymes" :imgSrc="require('@/assets/icons/logo/nursery-rhymes-logo.svg')"
+      alt="兒歌logo" />
+
+      <a
+      href=""
+      class="go-to-rhythm-and-movement-section"
+      v-scroll-to="'#rhythm-and-movement'"
+    >
+      直接前往律動
+    </a>
+
+      <SubSectionTitle title="熱門兒歌 Hot! Rhymes" :imgSrc="require('@/assets/icons/logo/sub-title-logo.svg')"
+      alt="熱門兒歌logo" />
+    <HotRhymes />
+    <SectionTitle title="手指謠 Finger Rhymes" :imgSrc="require('@/assets/icons/logo/finger-rhymes-logo.svg')"
+      alt="手指謠logo" />
+    <router-view />
+  </div>
 </template>
 
 <script>
 import BreadCrumb from "@/components/func-items/BreadCrumb.vue";
 import SectionTitle from "@/components/SectionTitle.vue";
+import SubSectionTitle from "@/components/SubSectionTitle.vue";
+import HotRhymes from "@/views/nursery-rhymes-and-finger-rhymes/HotRhymes.vue";
 
 export default {
-  components: { BreadCrumb,SectionTitle },
+  components: { BreadCrumb, SectionTitle,SubSectionTitle, HotRhymes },
   data() {
     return {};
   },
@@ -34,10 +42,10 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/scss/all.scss";
 
-div.container{
+div.container {
   width: 100%;
   height: 700px;
-  margin-top:80px;
+  margin-top: 80px;
   position: relative;
 }
 </style>
